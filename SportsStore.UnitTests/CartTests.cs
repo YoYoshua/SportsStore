c1c43ae5520e672b1005775b77ff2fc8aa0e5216
YoYoshua <marcinp32@gmail.com>
2018-10-17 22:07:59 +0200

    Implement cart function

---
+++ SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            //przygotowanie produktow testowych
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            //przygotowanie koszyka
+            Cart target = new Cart();
+
+            //dzialanie
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 2);
+            CartLine[] results = target.Lines.ToArray();
+
+            //asercje
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            //przygotowanie produktow testowych
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            //przygotowanie koszyka
+            Cart target = new Cart();
+
+            //dzialanie
+            target.AddItem(p1, 1);
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 5);
+            target.AddItem(p2, 7);
+            CartLine[] result = target.Lines.ToArray();
+
+            //asercje
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Quantity, 2);
+            Assert.AreEqual(result[1].Quantity, 12);
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            //przygotowanie produktow testowych
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            //przygotowanie koszyka
+            Cart target = new Cart();
+
+            //dzialanie
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 4);
+            target.RemoveLine(p2);
+            CartLine[] result = target.Lines.ToArray();
+
+            //asercje
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Product, p1);
+            Assert.AreEqual(result[0].Quantity, 1);
+            Assert.AreEqual(result[1].Product, p3);
+            Assert.AreEqual(result[1].Quantity, 4);
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            //przygotowanie produktow testowych
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            //przygotowanie koszyka
+            Cart target = new Cart();
+
+            //dzialanie
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+            decimal result = target.ComputeTotalValue();
+
+            //asercje
+            Assert.AreEqual(result, 450M);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            //przygotowanie produktow testowych
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            //przygotowanie koszyka
+            Cart target = new Cart();
+
+            //przygotowanie produktow w koszyku
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            //dzialanie: czyszczenie koszyka
+            target.Clear();
+
+            //asercje
+            Assert.AreEqual(target.Lines.Count(), 0);
+        }
+    }
+}
--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -34,7 +34,7 @@ namespace SportsStore.UnitTests
 
             //dzialanie
             ProductListViewModel result =
-                (ProductListViewModel)controller.List(2).Model;
+                (ProductListViewModel)controller.List(null, 2).Model;
 
             //asercje
             Product[] prodArray = result.Products.ToArray();
@@ -88,7 +88,7 @@ namespace SportsStore.UnitTests
             controller.PageSize = 3;
 
             //dzialanie
-            ProductListViewModel result = (ProductListViewModel)controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel)controller.List(null, 2).Model;
 
             //asercje
             PagingInfo pageInfo = result.PagingInfo;
@@ -96,6 +96,112 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(pageInfo.ItemsPerPage, 3);
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            //przygotowanie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            });
+
+            //przygotowanie kontrolera i 3-elementowej strony
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //dzialanie
+            Product[] result = ((ProductListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            //asercje
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            //przygotowanie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Jabłka"},
+                new Product {ProductID = 2, Name = "P2", Category = "Jabłka"},
+                new Product {ProductID = 3, Name = "P3", Category = "Śliwki"},
+                new Product {ProductID = 4, Name = "P4", Category = "Pomarańcze"},
+            });
+
+            //przygotowanie kontrolera
+            NavController target = new NavController(mock.Object);
+
+            //dzialanie: pobranie zbioru kategorii
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            //asercje
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Jabłka");
+            Assert.AreEqual(results[1], "Pomarańcze");
+            Assert.AreEqual(results[2], "Śliwki");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            //przygotowanie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Jabłka"},
+                new Product {ProductID = 4, Name = "P2", Category = "Pomarańcze"},
+            });
+
+            //przygotowanie kontrolera
+            NavController target = new NavController(mock.Object);
+
+            //przygotowanie kategorii do wybrania
+            string categoryToSelect = "Jabłka";
+
+            //działanie
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            //asercje
+            Assert.AreEqual(categoryToSelect, result);
+        }
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //przygotowanie imitacji repozytorium
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product {ProductID = 4, Name = "P4", Category = "Cat1" },
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3" }
+            });
+
+            //przygotowanie kontrolera
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //działanie
+            int res1 = ((ProductListViewModel)controller.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductListViewModel)controller.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductListViewModel)controller.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductListViewModel)controller.List(null).Model).PagingInfo.TotalItems;
+
+            //asercje
+            Assert.AreEqual(res1, 3);
+            Assert.AreEqual(res2, 1);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
         }
     }
 }
